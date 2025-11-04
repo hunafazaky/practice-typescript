c9d3adbd10e8c8ab29ad6bcfda80054f4eb0e9b5
hunafazaky <hunafa.zaky@gmail.com>
2025-11-04 22:48:44 +0700

    update

---
+++ truncate-a-string.ts
@@ -0,0 +1,6 @@
+// truncate a string based on limit given
+const truncateString = (text:string, limit:number) => {
+  return text.length > limit ? text.slice(0, limit) + "...":text
+}
+
+console.log(truncateString("hello", 3))
